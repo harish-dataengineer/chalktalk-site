39725bd76dacf655c90b2a8309c86633edcf8214
harish <mulaharishkumar@gmail.com>
2026-09-05 09:27:00 +0530

    updated firebase admins file

--- api/firebase-admin.js
+++ api/firebase-admin.js
@@ -1,4 +1,5 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
+import admin from "firebase-admin";
 
 if (!getApps().length) {
   initializeApp({
@@ -9,3 +10,5 @@ if (!getApps().length) {
     })
   });
 }
+
+export default admin;
