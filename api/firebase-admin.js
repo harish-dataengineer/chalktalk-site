ecddd6b8892e0121c60b5e292729e0a32acb90d6
harish <mulaharishkumar@gmail.com>
2026-09-05 09:19:31 +0530

    firebase changed

--- api/firebase-admin.js
+++ api/firebase-admin.js
@@ -1,13 +1,11 @@
-import admin from "firebase-admin";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
     })
   });
 }
-
-export default admin;
